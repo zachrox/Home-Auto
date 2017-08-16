7e0c7f8ed7d67cc81103911a6e489cf70052f55a
dmartin <dan.512@hotmail.co.uk>
2017-08-16 15:54:54 +0100

    google gauge and chart api added

--- Pages/BackGarden.php
+++ Pages/BackGarden.php
@@ -1,8 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" type="text/css" href="Style.css">
+    <meta charset="UTF-8">
+
+<script type="text/javascript" src="gauge.js"></script>
+   <script type="text/javascript">
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Temp', 0],
+          ['Humidity', 0],
+          ['Soil', 0],
+          ['Sun Light', 0]
+        ]);
+
+        var options = {
+          width: 400, height: 120,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 1000);
+        setInterval(function() {
+          data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 1000);
+        setInterval(function() {
+          data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+          chart.draw(data, options);
+        }, 1000);
+        setInterval(function() {
+          data.setValue(3, 1, Math.round(60));
+          chart.draw(data, options);
+        }, 1000);
+      }
+</script>
+
     <head>
-        <link rel="stylesheet" type="text/css" href="Style.css">
-        <meta charset="UTF-8">
         <title>Home Auto</title>
     </Head>
     <body>
@@ -13,6 +59,14 @@
                 <li><a href="#">Front Room</a></li>
                 <li><a href="#">Settings</a></li>
             </ul>
-        <div>
+        </div>
+
+        <div id="widget_box">
+            <p>Herb Garden Sensors</p>
+            <div id="chart_div" style="position: relative; display: inline-block; width: 400px; height: 120px;"></div>
+
+    
+        </div>
+
     </body>
 </html>
--- Pages/Main.php
+++ Pages/Main.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link rel="stylesheet" type="text/css" href="Style.css">
+<link rel="stylesheet" type="text/css" href="Style.css">
         <meta charset="UTF-8">
+
+    <head>
         <title>Home Auto</title>
     </Head>
     <body>
@@ -15,14 +16,5 @@
             </ul>
         </div>
 
-    <div id="widget_box">
-        <p>The date is: </p>
-
-        <?php echo gmdate('<p> d/m/Y <p/>'); ?> 
-
-    
-    <FORM METHOD=GET ACTION="textarea.php"> <INPUT TYPE=SUBMIT> </FORM>
-    </div>
-    </FORM>
     </body>
 </html>
