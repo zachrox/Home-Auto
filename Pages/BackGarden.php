35992938a5d8d507b7f4d675a0e7960dfbcb1fca
dmartin <dan.512@hotmail.co.uk>
2017-08-17 16:39:21 +0100

    sql

--- Pages/BackGarden.php
+++ Pages/BackGarden.php
@@ -50,7 +50,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["temp"];
+        echo $row["Temp"];
     }
 } else {
     echo "0 results";
