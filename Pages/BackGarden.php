e4924cca81e154561cc4ff029aeec4beb5646597
dmartin <dan.512@hotmail.co.uk>
2017-08-17 16:42:27 +0100

    sql

--- Pages/BackGarden.php
+++ Pages/BackGarden.php
@@ -32,31 +32,31 @@
 
         setInterval(function() {
           data.setValue(0, 1, <?php
-$servername = "localhost";
-$username = "root";
-$password = "ADMIN";
-$dbname = "8266Data";
+          $servername = "localhost";
+          $username = "root";
+          $password = "ADMIN";
+          $dbname = "8266Data";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+          // Create connection
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          // Check connection
+          if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+          } 
 
-$sql = "SELECT * FROM 8266Data.TempData ORDER BY id DESC LIMIT 1";
-$result = $conn->query($sql);
+          $sql = "SELECT * FROM 8266Data.TempData ORDER BY id DESC LIMIT 1";
+          $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo $row["Temp"];
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>);
+          if ($result->num_rows > 0) {
+              // output data of each row
+              while($row = $result->fetch_assoc()) {
+                  echo $row["Temp"];
+              }
+          } else {
+              echo "0 results";
+          }
+          $conn->close();
+          ?>);
           chart.draw(data, options);
         }, 1300);
         setInterval(function() {
