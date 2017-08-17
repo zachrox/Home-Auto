919cdcdd56fa51ab9cc1d82176b589914307514d
dmartin <dan.512@hotmail.co.uk>
2017-08-17 16:33:24 +0100

    sql attempt

--- Pages/BackGarden.php
+++ Pages/BackGarden.php
@@ -4,7 +4,32 @@
 <meta charset="UTF-8">
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "ADMIN";
+$dbname = "8266Data";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM 8266Data.TempData ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Temp: " . $row["temp"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 <script type="text/javascript">
       google.charts.load('current', {'packages':['gauge']});
       google.charts.setOnLoadCallback(drawChart);
